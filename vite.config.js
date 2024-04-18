647f785bf6f3f8740c7ef0f7f0ff5db0ccf15908
Andwar22 <and.war22@gmail.com>
2024-04-18 16:11:23 +0700

    trying

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'andwar22.online/',
+  base: '/andwar22.online',
   plugins: [
     vue(),
   ],
