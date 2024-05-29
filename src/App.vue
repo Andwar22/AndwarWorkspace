7022ca338647f84bcb7e78c24065e989ad9a268e
Andwar22 <and.war22@gmail.com>
2024-05-29 15:10:02 +0700

    design awal main page

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,7 @@
 <script setup>
-  import JudulPath from './components/JudulPath.vue';
-  import ItemPath from './components/ItemPath.vue';
-  import IconLampu from './components/icons/IconLampu.vue';
+  import MainPath from './components/main-page/MainPath.vue';
 </script>
 
 <template>
-  <div class="main-page">
-      <div class="choose-path">
-        <JudulPath msg="Ate lapo yooo??" />
-        
-        <div class="wrap-item-path">
-          <ItemPath>
-            <template #icon>
-              <IconLampu />
-            </template>
-            <template #title>Kerjo</template>
-            <a href="/layout/work-space.html">KERJO</a>
-          </ItemPath>
-
-          <ItemPath>
-            <template #icon>
-              <IconLampu />
-            </template>
-            <template #title>Dodolan</template>
-            <a href="">KERJO</a>
-          </ItemPath>
-
-          <ItemPath>
-            <template #icon>
-              <IconLampu />
-            </template>
-            <template #title>Dolanan</template>
-            <a href="">KERJO</a>
-          </ItemPath>
-        </div>
-      </div>
-    </div>
+  <MainPath />
 </template>
---
+++ src/components/icons/IconController.vue
@@ -0,0 +1,9 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-device-nintendo" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" fill="none" d="M0 0h24v24H0z" />
+        <path stroke="currentColor" d="M10 20v-16h-3a4 4 0 0 0 -4 4v8a4 4 0 0 0 4 4h3z" />
+        <path stroke="currentColor" d="M14 20v-16h3a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-3z" />
+        <circle stroke="currentColor" fill="currentColor" cx="17.5" cy="15.5" r="1" />
+        <circle stroke="currentColor" fill="currentColor" cx="6.5" cy="8.5" r="1" />
+    </svg>
+</template>
